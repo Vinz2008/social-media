f9ca7df8129701e80bd6de1a1a891caefdcb71d6
Vinz2008 <vincentbidarddelanoe@gmail.com>
2023-02-15 22:35:22 +0100

    work on sidebar

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,53 @@
 <script>
-	import SocialMedia from './SocialMedia.svelte';
-	import Profile from './Profile.svelte';
-	import NavBar from './NavBar.svelte';
-	import { auth, googleProvider } from './firebase';
+	import SocialMedia from './lib/SocialMedia.svelte';
+	import Profile from './lib/Profile.svelte';
+	import NavBar from './lib/NavBar.svelte';
+	import { auth, googleProvider } from './lib/firebase';
 	import { authState } from 'rxfire/auth';
 	let user;
 	const unsubscribe = authState(auth).subscribe(u => user = u);
 	function login() {
         auth.signInWithPopup(googleProvider);
     }
+	let page_nb;
+	function change_page_nb(event){
+		const { page } = event.detail;
+		page_nb = page;
+		console.log("changed really page ", page);
+	}
 </script>
 
-<section>
-	<NavBar/>
-	<main2>
+<main>
+<NavBar on:goTo={change_page_nb}/>
+<outNavBar>
+{#if page_nb == 0}
+{#if user}
+<SocialMedia uid={user.uid} />
+{/if}
+{/if}
+
+{#if page_nb == 2}	
 	{#if user}
 		<Profile {...user} />
 		<button on:click={ () => auth.signOut() }>Logout</button>
 		<hr>
-		<SocialMedia uid={user.uid} />
 	{:else}
 		<button on:click={login}>
 			Signin with Google
 		</button>
 	{/if}
-	</main2>
-</section>
+{/if}
+</outNavBar>
+</main>
 
 <style>
-	main {
+	section {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
 	}
-	main2 {
+	outNavBar {
 		position:absolute;
   		overflow:auto;
 		padding-left: 10em;
--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
+import './app.css'
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+	target: document.getElementById('app'),
 });
 
 export default app;
